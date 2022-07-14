4a7b51115c86e563f332ccbf11265efe6bae57f1
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-07-14 10:25:20 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,17 @@
+// Write a program to print all odd numbers between 1 to 100 using 'continue'
+
+class Exercise1 {
+
+	public static void main(String args[]) {
+
+int i=1;
+    for(i=1;i<=100;i++)
+      {
+        if(i%2==0)
+          continue;
+        else
+          System.out.println(i);
+      }
+	}
+
+}
