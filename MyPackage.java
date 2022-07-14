42e08d44d8ac6e25f23cbc38e3345fc12d7a0200
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-07-14 10:40:59 +0530

    Add files via upload

---
+++ MyPackage.java
@@ -0,0 +1,20 @@
+/*  JFM1T6_Assignment5:
+    Write a class "MyClass" in package "mypack" and access it from other class outside the package "mypack".
+
+    Sample Output:
+    Welcome to Packages
+*/
+
+//create a package name mypack, inside a folder called mypack, and create a class MyClas. In that create Display method and  print message
+
+//outside folder  import the mypack  and call Dislay method 
+import mypack.MyClass;
+public class MyPackage {
+public static void main(String[] args){
+  MyClass c =new MyClass();
+  c.myclass();
+}
+}
+
+//Good Job!
+//Please push your assignment to github. If you have pused it, forget this comment.
