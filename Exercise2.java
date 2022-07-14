b3e9be835847339be2533cee281aeab53b594798
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-07-14 10:35:44 +0530

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,26 @@
+//  Write a program to create a two dimensional array with duplicate elements and print only the duplicate elements.
+class Exercise2 {
+	public static void main(String args[]) {
+  int a[][]={{1,1,3},{2,3,6},{7,8,8}};
+  int b[]={-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    for(int i=0;i<a.length;i++)
+      {
+        for(int j=0;j<a[i].length;j++)
+          {
+            for(int k=0;k<b.length;k++)
+              {
+                if(a[i][j]!=b[k]&& b[k]==-1)
+                {
+                  b[k]=a[i][j];
+                  break;
+                }
+                else if(a[i][j]==b[k])
+                {
+                  System.out.println(a[i][j]);
+                  break;
+                }
+              }
+          }
+      }
+   }
+}
