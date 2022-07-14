798a63c7d4d85d0048786170aa62c66d37a47547
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-07-14 10:38:24 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,13 @@
+// Write a program for printing only even elements of an array using for-each loop 
+class Exercise1 {
+
+	public static void main(String args[]) {
+int arr[]={1,2,3,4,5,7,8};
+    for(int i=0;i<arr.length;i++){
+      if(arr[i]%2==0){
+        System.out.print(arr[i]+" ");
+      }
+    }
+		
+   }
+}
