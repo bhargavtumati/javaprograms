749b2c8292e414efd5f9638ceb81c90c6111546b
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 21:17:31 +0530

    Add files via upload

---
+++ JFM1T7_Exercise2-bhargavtumati/Exercise1.java
@@ -0,0 +1,24 @@
+//  Write a Car class and initialize its objects using Constructor.
+//Create a class which contains the main method 
+class Exercise1 {
+
+	public static void main(String args[]) {
+
+	//Write code here to create two Car objects of your choice
+	Car c1 = new Car();
+    System.out.println("car model and price is "+c1.model+" "+c1.price);
+  
+  }
+}
+	
+//Create Car class, it's attributes, constructor and method
+class Car {  
+String model;
+int price;
+ public Car(){
+   model = "Suv200";
+   price = 500000;
+
+ }
+}
+		    
---
+++ JFM1T7_Exercise2-bhargavtumati/Sample1 .java
@@ -0,0 +1,28 @@
+//  Complete and execute java program to initialize Employee object using Constructor
+
+class Sample1{  
+int empid;  
+String name; 
+double salary;
+
+//Default constructor
+Sample1(){
+//Initialize here
+empid=1;
+  name="bhargav";
+    salary=40000;
+}
+ 
+//Method to display the value of id, name and salary  
+void display(){
+System.out.println(empid+" "+name+" "+salary);
+}  
+  
+public static void main(String args[]){  
+//Create object  
+Sample1 emp1 =new Sample1();
+//Display values of the object by calling the method  
+emp1.display();
+}  
+}
+		    
