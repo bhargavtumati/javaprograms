050ff850d888d0a4864b52f39b6e531c8a262d6b
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 21:06:57 +0530

    Add files via upload

---
+++ JFM1T6_Assignment-bhargavtumati/Mean.java
@@ -0,0 +1,56 @@
+/* JFM1T6_Assignment3:
+
+   Write a method that takes any numbers as arguments/parameters and return the mean value back to main method.
+   Prompt the user for the  values to be input from the terminal.
+
+   Sample input:
+   num1 = 3
+   num2 = 2
+   num3 = 4
+
+   Expected Output:
+   result = 3
+   
+*/
+
+//main method
+
+//declare variables and initialize sum=0
+
+//create scanner object
+
+//take input from user
+
+//check the for loop for number of values entered in input
+//calculate mean
+
+//print result
+ import java.util.Scanner;
+
+public class Mean {
+  float mean (Scanner s)
+  {
+    int sum=0;
+    int n=0;
+    System.out.println("Enter the elements to find their Mean\nEnter a character(except integers) to terminate Input or press ctrl+d!!");
+    while(s.hasNextInt())
+      {
+        n++;
+        sum+=s.nextInt();
+      }
+    return ((float)sum/n);
+  }
+
+public static void main(String args[])
+  {
+    float avg;
+    Mean m=new Mean();
+    Scanner sc=new Scanner(System.in);
+    avg=m.mean(sc);
+    System.out.println("result = "+avg);
+  }
+}
+
+//Good Job!
+//Please maintain proper comments in 
+//Please push your assignment to github. If you have pused it, forget this comment.
---
+++ JFM1T6_Assignment-bhargavtumati/Power.java
@@ -0,0 +1,60 @@
+/* JFM1T6_Assignment2:
+   Write a method that takes 2 numbers as arguments/parameters and returns the value of num2 to the power of num1 back to the main method.
+   Prompt the user for the base and power values to be input from the terminal.
+
+   Sample input:
+   num1 = 3
+   num2 = 2
+    
+   Expected Output:
+   result = 9
+   
+*/
+import java.util.Scanner;
+public class Power {
+
+//main method
+int power(int n,int p)
+  {
+    int r=1;
+     if(n>=0 && p==0)
+    {
+      return 1;
+    }
+    else if(n==0 && p>=1)
+    {
+      return 0;
+    }
+    else
+    {
+      while(p!=0)
+        {
+          r*=n;
+          p--;
+        }
+      return r;
+    }
+  }
+
+public static void main(String[] args){
+//declare variables n,p,r and initialize r as one.
+int n,p,r=1;
+//create scanner class object
+Scanner sc= new Scanner(System.in);
+//take input from user
+
+//check if (n>=0&&p==0) if true then it prints 1 else if check (n==0&&p>=1) if true then print zero else multiply r*n
+
+//print result
+System.out.print("Enter base:");
+    n=sc.nextInt();
+    System.out.print("Enter exponent:");
+    p=sc.nextInt();
+    Power obj=new Power();
+    r=obj.power(n,p);
+    System.out.println("Result: "+r);
+    }
+}
+
+//Good Job!
+//Please push your assignment to github. If you have pused it, forget this comment.
