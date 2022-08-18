f70c09e31e279a761ed50914b800d0ccf4770c80
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 20:12:02 +0530

    Add files via upload

---
+++ JFM1T4_Exercise1-bhargavtumati/Exercise1.java
@@ -0,0 +1,18 @@
+/* Write a program to check whether a number is positive or negative
+Test Data 1: x=31
+Expected Output: Number is positive
+Test Data 2: x=-31
+Expected Output: Number is negative
+*/
+
+class Exercise1 {
+
+	public static void main(String args[]) {
+int x=31;
+    if(x>0)
+      System.out.println("Number is positive");
+else
+      System.out.println("Number is negative");
+	}
+
+}
