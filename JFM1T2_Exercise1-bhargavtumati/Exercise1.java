1536b613218527d090761231663d4e1e8cad3a82
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 19:32:46 +0530

    Create Exercise1.java

---
+++ JFM1T2_Exercise1-bhargavtumati/Exercise1.java
@@ -0,0 +1,9 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+public class Exercise1 {
+public static void main(String args[]) {
+ System.out.println("Bhargav Tumati");
+  System.out.println("T Adrushta Rao");
+    System.out.println("bhargav.tumati15@gmail.com");
+}
+}
