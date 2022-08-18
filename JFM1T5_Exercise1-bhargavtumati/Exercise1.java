2631c9ac74330bf9631b0e04e30cf48fc7bb449c
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 20:54:42 +0530

    Add files via upload

---
+++ JFM1T5_Exercise1-bhargavtumati/Exercise1.java
@@ -0,0 +1,13 @@
+//  Write a program to print the elements of an array present on even position
+class Exercise1 {
+
+	public static void main(String args[]) {
+int arr[]= {1,2,3,4,5,6,7};
+    for(int i=1;i<arr.length;i++){
+      if(i%2==0){
+        System.out.println(i);
+      }
+    }
+		    
+   }
+}
---
+++ JFM1T5_Exercise1-bhargavtumati/Sample1.java
@@ -0,0 +1,28 @@
+// Complete and execute java Program for finding smallest/minimum element in a single dimensional array
+import java.util.*;
+class Sample1 {
+
+	public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+		//Initialize array  
+        	int[] arr = new int[5];
+    System.out.println("Enter elements");
+    for(int i=0;i<=4;i++){
+      arr[i]=sc.nextInt();
+    }
+
+        	//Initialize min with first element of array.  
+        	int min = arr[0];  
+
+        	//Loop through the array  
+        	for(int i=0;i<=arr.length-1;i++) {//Intialization;Condition;Increment) {  
+
+            	//Compare elements of array with min  
+           	if(arr[i]<=min){//Test Condition)  
+               	//Write code to store in min if array element is less than it.   
+       min=arr[i];
+            }  
+          }
+     System.out.println("Smallest element present in given array: " + min); 
+   }
+}
