fbce1bb815da13b728daf00833d79588afbcd154
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 21:11:02 +0530

    Add files via upload

---
+++ JFM1T6_Exercise2-bhargavtumati/Sample1.java
@@ -0,0 +1,21 @@
+//  Complete and execute java program using method to calculate square of a number
+import java.util.Scanner;
+class Sample1 {
+
+// Create a method named square which accepts a number as parameter and returns square of that number
+ int square(int x){
+  return (x*x);
+ }
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    Sample1 obj = new Sample1();
+    // store returned value to result
+    result = obj.square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+  
+
+}
