7b8a3e01639e92a5e6d617977ab7e9f93033c2b8
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 20:03:29 +0530

    Add files via upload

---
+++ JFM1T3_Exercise1-bhargavtumati/Exercise1.java
@@ -0,0 +1,16 @@
+// Write a program to initialize a char variable and then store it in an int variable. Print the int variable's value on console.
+
+class Exercise1 {
+	public static void main(String args[]) {
+   // create char variables
+		char a = '5';
+    char b = 'C';
+// convert char variables to int
+    // ASCII value of characters is assigned
+    int num1 = Character.getNumericValue(a);
+    int num2 = Character.getNumericValue(b);
+// print the values
+    System.out.println(num1);
+    System.out.println(num2);
+	}
+}
