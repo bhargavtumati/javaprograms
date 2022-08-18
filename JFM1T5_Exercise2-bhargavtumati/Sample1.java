b7b4f0698651582b795c71ecae31d7f0dd56233d
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 21:02:21 +0530

    Add files via upload

---
+++ JFM1T5_Exercise2-bhargavtumati/Sample1.java
@@ -0,0 +1,17 @@
+// Complete and execute java Program for printing only odd elements of a single dimensional array
+class Sample1 {
+
+	public static void main(String args[]) {
+
+		//Initialize array  
+        	int [] arr = new int [] {25, 11, 7, 75, 56}; 
+
+        	//Loop through the array using for-each loop
+        	for (int i:arr)
+          {
+           	if(i%2!=0){//Test Condition)  
+               	System.out.print(i+" ");//Write print statement here
+        }  
+          }
+   }
+}
