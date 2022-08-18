9e7722d3d86732bafd1e78ff57b2774400fa9b04
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 18:21:32 +0530

    Add files via upload

---
+++ Exercise3.java
@@ -0,0 +1,10 @@
+// Write a program to initialize your marks for 3 subjects and print the highest marks
+
+class Exercise3 {
+public static void main(String[] args){
+  int x=70;
+  int y=80;
+  int z=90;
+  System.out.println("x is highest "+(x>y&&x>z)+ " \ny is highest "+(y>x&&y>z)+ "\nz is highest "+(z>x&&z>y));
+}
+}
--- Sample1.java
+++ Sample1.java
@@ -1,10 +1,21 @@
-// Write a program to print your name
-
-public class Sample1 {
-
-	public static void main(String args[]) {
-
-	//Print your name in Console
-	System.out.println("Bhargav Tumati");
-	}
+// Complete the execute the java Program for Logical Operators
+public class Sample1{
+    
+ public static void main(String[] args) {
+     
+  //Variables Definition and Initialization
+  boolean x = true;
+  boolean y = false;
+  
+  //Logical AND
+  System.out.println("x&&y="+(x&&y));//Enter code here to perform Logical AND operation between x and y);
+  
+  //Logical OR
+  System.out.println("X||y="+(x||y));//Enter code here to perform Logical OR operation between x and y);
+  
+  //Write Logical NOT for x in println statement below
+  System.out.println("!x"+!x);
+  
+  }
 }
+
