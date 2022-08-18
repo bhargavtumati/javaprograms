26b622558b553b1572e157c0999e147fe9edbf35
Bhargav Tumati <94704486+bhargavtumati@users.noreply.github.com>
2022-08-18 19:55:13 +0530

    Add files via upload

---
+++ JFM1T2_Exercise2-bhargavtumati/Exercise1.java
@@ -0,0 +1,11 @@
+// Write a program to print the remainder when x is divided by y where x=100 and y=4
+
+public class Exercise1 {
+public static void main (String[] args){
+  int x=100;
+    int y=4;
+    int z;
+  z=x%y;
+  System.out.println("remainder when x divideded by y is" +z);
+}
+}
